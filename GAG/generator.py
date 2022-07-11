da9e7636635b6ad16bd586bc617f85471bd35da4
JGH <siblue202@gmail.com>
2022-07-11 14:48:08 +0900

    add key value generator

---
+++ GAG/generator.py
@@ -0,0 +1,11 @@
+from random import randint
+
+max_value = 10000
+key = [randint(0, max_value) for p in range(0, 100)]
+value = [randint(1, max_value) for p in range(0, 100)]
+
+f = open("text.txt", 'w')
+f.write("%d\n" % max_value)
+for i in range(100):
+    data = "<%d,%d> " % key[i], value[i]
+    f.write(data)
