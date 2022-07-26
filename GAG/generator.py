a8e24414bd6dc28c20ad610e87947fd29880948c
JGH <siblue202@gmail.com>
2022-07-26 09:57:12 +0900

    add mpi_gag.c

--- GAG/generator.py
+++ GAG/generator.py
@@ -7,5 +7,5 @@ value = [randint(1, max_value) for p in range(0, 100)]
 f = open("text.txt", 'w')
 f.write("{}\n".format(max_value))
 for i in range(100):
-    data = "<{},{}> ".format(key[i], value[i])
+    data = "<{},{}>\n".format(key[i], value[i])
     f.write(data)
