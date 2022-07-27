7d8acb95b67aaf59365748323fbb027deb53f589
JGH <siblue202@gmail.com>
2022-07-27 15:52:12 +0900

    minor fix in generator.py

--- GAG/generator.py
+++ GAG/generator.py
@@ -1,11 +1,11 @@
 from random import randint
 
 max_value = 10000
-key = [randint(0, max_value) for p in range(0, 1000000)]
-value = [randint(1, max_value) for p in range(0, 1000000)]
+key = [randint(0, max_value) for p in range(0, 10000000)]
+value = [randint(1, max_value) for p in range(0, 10000000)]
 
 f = open("text.txt", 'w')
 f.write("{}\n".format(max_value))
-for i in range(1000000):
+for i in range(10000000):
     data = "<{},{}>\n".format(key[i], value[i])
     f.write(data)
