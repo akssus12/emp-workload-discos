993db9856bb9c942f5b2fadc0c370fa614debcf6
JGH <siblue202@gmail.com>
2022-08-08 18:41:21 +0900

    add shell script to run workload

---
+++ AG/c/running_mpi_ag.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# $1 : number of using cores
+# $2 : target file neme
+# line : lines of target file
+
+line=$(wc -l < $2)
+echo "${line}"
+
+mpirun -np $1 ./mpi_ag -hostfile ./my_hostfile $2 $line
