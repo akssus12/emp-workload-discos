415e27d640e8d16a95e8512dc5f341e9485b0406
JGH <siblue202@gmail.com>
2022-09-23 15:35:08 +0900

    wordcount workload with lower ccn

--- WordCount/c/running_mpi_wc.sh
+++ WordCount/c/running_mpi_wc.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-mpirun -np $1 -hostfile ./my_hostfile ./mpi_wc $2
+# $1 : num of processes
+# $2 : target file neme
+# line : lines of target file
+
+line=$(wc -l < $2)
+echo "${line}"
+
+mpirun -np $1 -hostfile ./my_hostfile ./mpi_wc $2 $line
